1d8768039ac8a6e312f399a8db89ac1a3f590896
Anay Dandekar <anaydbcp@gmail.com>
2026-07-15 13:02:40 -0700

    Added basic functional UI to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,67 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const Sender = Object.freeze({
+    USER: "user",
+    BOT: "bot"
+});
+
+class Message {
+    constructor(text, sender) {
+        this.text = text;
+        this.sender = sender;
+    }
+}
+
+const messages = ref([]);
+
+const messageInput = ref("");
+
+function sendMessage() {
+    if (messageInput.value != "") {
+        messages.value.push(
+            new Message(
+                messageInput.value,
+                Sender.USER
+            )
+        );
+        messageInput.value = "";
+
+        // placeholder until actual message response from bot
+        messages.value.push(
+            new Message(
+                "This is a placeholder response from the bot",
+                Sender.BOT
+            )
+        );
+    }
+}
+</script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+    <div class="py-10 px-50">
+        <!-- page hero -->
+        <p class="text-7xl text-center">How can I help?</p>
+    
+        <!-- show message history -->
+        <div v-for="message in messages">
+            <div v-if="message.sender == Sender.BOT" class="chat chat-start">
+                <div class="chat-bubble">{{ message.text }}</div>
+            </div>
+            <div v-if="message.sender == Sender.USER" class="chat chat-end">
+                <div class="chat-bubble">{{ message.text }}</div>
+            </div>
+        </div>
+    
+        <!-- input to type messages -->
+        <div class="flex justify-center mt-8">
+            <input
+                type="text"
+                class="input"
+                placeholder="Type your question here..."
+                v-model="messageInput"
+                @keydown.enter="sendMessage"       
+            >
+        </div>
+    </div>
 </template>
-
-<style scoped></style>
